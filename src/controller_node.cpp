e04a25bddd21ad8e37a7d86ab1610f09075c85b7
Elio-in-summer <3180106063@zju.edu.cn>
2024-05-17 12:25:12 +0800

    "modify perch success param"

--- src/controller_node.cpp
+++ src/controller_node.cpp
@@ -724,7 +724,7 @@ int main(int argc, char **argv)
             double dis_xy = sqrt(pow(uav_in_arm_frame.pose.position.x, 2) + pow(uav_in_arm_frame.pose.position.y, 2));
             double dis_z = uav_in_arm_frame.pose.position.z;
             // ! Here the param offset should be set according to the real situation
-            if(dis_xy < 0.3 && 0 <dis_z < 0.1){
+            if(dis_xy < 0.3 && -0.05 <dis_z < 0.1){
                 perch_state = 2;
             }
             else if (dis_z > 0.3 && execute_flag == -1){
